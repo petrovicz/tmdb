6d8a788376141d5792dae2a6489a98552c3a37ef
Petrovicz <benedek.petrovicz@gmail.com>
2022-07-05 14:20:28 +0200

    Adding fetch functionality

--- TmdbBully/Controllers/TmdbController.cs
+++ TmdbBully/Controllers/TmdbController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using TmdbBully.Managers;
 
 namespace TmdbBully.Controllers
 {
@@ -6,16 +7,21 @@ namespace TmdbBully.Controllers
 	public class TmdbController : ControllerBase
 	{
 		private readonly ILogger<TmdbController> _logger;
+		private readonly IConfiguration _configuration;
 
-		public TmdbController(ILogger<TmdbController> logger)
+		public TmdbController(ILogger<TmdbController> logger, IConfiguration configuration)
 		{
 			_logger = logger;
+			_configuration = configuration;
 		}
 
 		[HttpGet]
 		[Route("fetch")]
 		public string Fetch()
 		{
+			var fetchManager = new FetchManager(_configuration["ApiKey"]);
+			Task.Run(() => fetchManager.Process());
+
 			return "Fetching top 120 movies...";
 		}
 
---
+++ TmdbBully/Managers/FetchManager.cs
@@ -0,0 +1,57 @@
+﻿namespace TmdbBully.Managers
+{
+	public class FetchManager
+	{
+		private readonly string _apiKey;
+
+		public FetchManager(string apiKey)
+		{
+			_apiKey = apiKey;
+		}
+
+		public async Task Process()
+		{
+			await FetchAllData();
+		}
+
+		#region Fetching
+
+		private async Task FetchAllData()
+		{
+			var tasks = new List<Task>();
+
+			for (int i = 1; i < 7; i++)
+			{
+				tasks.Add(FetchPage(i));
+			}
+
+			await Task.WhenAll(tasks);
+		}
+
+		private async Task FetchPage(int page)
+		{
+			using var httpClient = new HttpClient();
+			var client = new ApiManager(httpClient, _apiKey);
+			var response = await client.GetTopRatedAsync(page);
+
+			foreach (var movie in response.Results)
+			{
+				// Each could run at the same time, but TMDB blocks the requests
+				// due to the high amount in a short period of time
+				await FetchMovieDetailsAndCredits(movie.Id ?? 0);
+			}
+		}
+
+		private async Task FetchMovieDetailsAndCredits(int id)
+		{
+			using var httpClient = new HttpClient();
+			var client = new ApiManager(httpClient, _apiKey);
+
+			var movieDetailsAndCredits = await client.GetMovieDetailsAndCreditsAsync(id);
+			var directorCrew = movieDetailsAndCredits.Credits.Crew.FirstOrDefault(c => c.Job == "Director");
+			var directorPerson = await client.GetPersonDetailsAsync(directorCrew?.Id ?? 0);
+		}
+
+		#endregion
+	}
+}
